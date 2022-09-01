f91902a547dc33485f8b64d74fee85fe59bf4a8f
zhangtime-abc <55736124+zhangtime-abc@users.noreply.github.com>
2022-09-01 20:01:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ export const config = {
     TEMPLATE_ID: "6QBcYvBFGjracJSldT2s-HgWY5SShkPGOcdNOjKLIes",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "	oqF9qtknZVaP_YWE65gULntSjyqJycdmFHO6QGY61WU",
+    CALLBACK_TEMPLATE_ID: "oqF9qtknZVaP_YWE65gULntSjyqJycdmFHO6QGY61WU",
 
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
     // [{
@@ -52,12 +52,12 @@ export const config = {
     // tpye必须填！ 只能 “生日” 和 “节日” 二选一!
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
-    FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
-      {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
-      {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
-      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
-    ],
+//     FESTIVALS: [
+//       {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
+//       {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
+//       {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
+//       {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
+//     ],
     // 限制重要节日的展示条目, 需要填写数字; 
     // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
     // 如果为0, 则默认展示全部
