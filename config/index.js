f9fc8f963f1f8586e48aac2218f4033596591dc0
zhangtime-abc <55736124+zhangtime-abc@users.noreply.github.com>
2022-09-01 20:33:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -26,13 +26,13 @@ export const config = {
     // }]
     USERS: [
       {name: '晚上好!亲爱的张运征同学:', id: "oBhNR529EqTpM3EgAyEWywsGuKRw"}, 
-      {name: '晚上好!亲爱的张同学:', id: "oBhNR5wKra0ysBFOf5rFVJB7AvPw"}, 
-//       {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
-//       {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
+      {name: '晚上好!亲爱的张同学:', id: "oBhNR5wKra0ysBFOf5rFVJB7AvPw"},  
+      // {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
+      // {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '张运征',id: "oBhNR5wKra0ysBFOf5rFVJB7AvPw"}, 
+      {name: '自己',id: "oBhNR5wKra0ysBFOf5rFVJB7AvPw"}, 
     ],
      
     /**
@@ -52,23 +52,23 @@ export const config = {
     // tpye必须填！ 只能 “生日” 和 “节日” 二选一!
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
-//     FESTIVALS: [
-//       {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
-//       {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
-//       {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
-//       {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
-//     ],
+    FESTIVALS: [
+      // {"type": "生日", "name": "老婆", "year": "198", "date": "09-02"},
+      // {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
+      // {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
+      // {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
+    ],
     // 限制重要节日的展示条目, 需要填写数字; 
     // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
     // 如果为0, 则默认展示全部
-    //FESTIVALS_LIMIT: 3,
+    FESTIVALS_LIMIT: 3,
 
     /** 日期相关 */
 
-//     // 在一起的日子，格式同上
-//     LOVE_DATE: "2015-05-01",
-//     // 结婚纪念日
-//     MARRY_DATE: "2020-01-04",
+    // 在一起的日子，格式同上
+    // LOVE_DATE: "2015-05-01",
+    // // 结婚纪念日
+    // MARRY_DATE: "2020-01-04",
 
 
     /** 每日一言 */
